8cbd1bf81e649caebec896b9aabbfa21bc8e06f2
sara burchi colonna <burchicolonnasara@gmail.com>
2025-05-14 19:02:34 +0200

    contact section almost done

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../contact.css";
+
+export default function Contact() {
+  return(
+    <div className="contact">
+      <h1>Contact Me :</h1>
+      <a href="https://www.linkedin.com/in/sburchi/" target="_blank" rel="noopener noreferrer">
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+      <a href="https://github.com/sarabc12" target="_blank" rel="noopener noreferrer">
+        <i class="fa-brands fa-github"></i>
+      </a>
+    </div>
+  )
+}
