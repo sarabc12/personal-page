5ebcec048d6ab8ebeb502a09b18416d5c4e1e5c7
sara burchi colonna <burchicolonnasara@gmail.com>
2025-05-14 17:44:27 +0200

    project section finished

--- src/components/WebPages.js
+++ src/components/WebPages.js
@@ -1,19 +1,41 @@
-import React from "react";
-import "../projects.css"
-
+import React, { useState } from "react";
+import "../projects.css";
 
 export default function WebPages() {
-  return(
+  const [hoveredCard, setHoveredCard] = useState(null);
+  const handleMouseEnter = (card) => setHoveredCard(card);
+  const handleMouseLeave = () => setHoveredCard(null);
+
+  return (
     <div className="webpages">
-      <h1>web pages</h1>
       <div className="webpages-cards">
-        <a href="https://www.cinebox.space/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.cinebox.space/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="card"
+          onMouseEnter={() => handleMouseEnter("cinebox")}
+          onMouseLeave={handleMouseLeave}
+        >
           <img src="images/cinebox.jpeg" alt="cinebox"></img>
+          {hoveredCard === "cinebox" && (
+            <div className="overlay">Cinebox is a movie discovery web app built with Ruby on Rails, PostgreSQL, and Bootstrap. It integrates with The Movie Database API to display dynamic movie content with search and favorite functionality. Users can explore trending films, view details, and curate a personalized favorites list.</div>
+          )}
         </a>
-        <a href="https://airbnbcopy-c389cbe529d1.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://airbnbcopy-c389cbe529d1.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="card"
+          onMouseEnter={() => handleMouseEnter("airbnb")}
+          onMouseLeave={handleMouseLeave}
+        >
           <img src="images/airbnbclone.png" alt="airbnbclone"></img>
+          {hoveredCard === "airbnb" && (
+            <div className="overlay">Airbnb Clone – As my first project at Le Wagon Web Development Bootcamp, I built a full-stack Airbnb-inspired web application. This platform allows users to browse listings, book stays, and manage their own properties. Using Ruby on Rails, PostgreSQL, and simple front-end styling with HTML, CSS, and JavaScript.</div>
+          )}
         </a>
       </div>
     </div>
-  )
+  );
 }
