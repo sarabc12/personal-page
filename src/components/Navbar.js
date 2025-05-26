ef12e00bf34b5e3447818167337d8864745e5ea8
sara burchi colonna <burchicolonnasara@gmail.com>
2025-05-26 15:20:37 +0200

    done with navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,34 @@
 import React from "react";
 import "../navbar.css";
-import { Link } from "react-router-dom";
-import { Link as ScrollLink} from "react-scroll";
+import { Link, useLocation } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 
 export default function Navbar() {
-  return(
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
+  return (
     <div className="Navbar">
       <div className="navbar-container">
-        <a className="navbar-logo" href="root">
+        <div className="navbar-logo">
           <Link to="/">
             <img src="imagelogo.png" alt="s-logo" />
           </Link>
-        </a>
-        <div className="buttons-container">
-          <ScrollLink to="about" smooth={true} duration={1000} offset={-70} className="btn">About Me</ScrollLink>
-          <ScrollLink to="project" smooth={true} duration={1000} className="btn">Project</ScrollLink>
-          <ScrollLink to="contact" smooth={true} duration={1000} className="btn">Contact me</ScrollLink>
         </div>
 
+        {isHomePage && (
+          <div className="buttons-container">
+            <ScrollLink to="about" smooth={true} duration={1000} offset={-70} className="btn">
+              About Me
+            </ScrollLink>
+            <ScrollLink to="project" smooth={true} duration={1000} className="btn">
+              Project
+            </ScrollLink>
+            <ScrollLink to="contact" smooth={true} duration={1000} className="btn">
+              Contact me
+            </ScrollLink>
+          </div>
+        )}
       </div>
     </div>
   );
